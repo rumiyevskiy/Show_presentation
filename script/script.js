711b31d3580efa97a230d01ebda1e8cdf301604b
rumiyevskiy <rumiyevskiy@gmail.com>
2025-05-15 10:10:12 +0300

    modified:   index.html 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,17 @@
 document.getElementById("showBtn").addEventListener("click", () => {
     const frame = document.getElementById("iframe-wrapper");
     frame.style.display = "block";
-  });
+});
+  
+
+// document.querySelectorAll(".showBtn").forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const targetId = btn.getAttribute("data-target");
+//     const frame = document.getElementById(targetId);
+//     if (frame) {
+//       frame.style.display = "block";
+//     }
+//   });
+// });
+
   
